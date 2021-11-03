430e92bd0e55acc51c35cbb5448e8247fe9ec4bf
chamRun <chrm@aut.ac.ir>
2021-11-03 17:13:43 +0330

    XNOR module done

--- LabFour.v
+++ LabFour.v
@@ -9,3 +9,28 @@
    end
 
   endmodule
+  
+  
+  module my_xnor(a,b,c,d, x);
+	
+	input a, b, c, d;
+	output x;
+	
+	wire e, f, g, h;
+	wire not_a, not_b, not_c;
+	
+	not g1(not_a, a);
+	not g2(not_b, b);
+	not g3(not_c, c);
+	
+	and g4(e, not_a, not_b, not_c);
+	and g5(f, not_a, b, c);
+	and g6(g, a, not_b, c);
+	and g7(h, a, b, not_c);
+	
+	or g8(x, e, f, g, h);
+	
+  endmodule
+	
+	
+	
