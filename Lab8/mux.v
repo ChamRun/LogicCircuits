b5c6fb8ff0e4f5ea89fa7f29b17778f01ab59bbb
chamRun <chrm@aut.ac.ir>
2021-12-05 20:30:13 +0330

    mux completed

--- Lab8/mux.v
+++ Lab8/mux.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    20:06:48 12/05/2021 
+// Create Date:    20:23:59 12/05/2021 
 // Design Name: 
-// Module Name:    ArithmeticUnit 
+// Module Name:    mux 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,14 +18,28 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module ArithmeticUnit(
-    input[1:0] a,
-    input[1:0] b,
-    output[3:0] multiply,
-    output[3:0] add
+module mux(
+	input [3:0] w3,
+	input [3:0] w2,
+	input [3:0] w1,
+	input [3:0] w0,
+	input [1:0]	s,
+	output [3:0] y
     );
 
-    assign multiply = a * b;
-    assign add = a + b;
+
+
+    if (s == 0) begin
+        assign y = w0;
+    end
+    else if (s == 1) begin
+        assign y = w1;
+    end
+    else if (s == 2) begin
+        assign y = w2;
+    end
+    else if (s == 3) begin
+        assign y = w3;
+    end
 
 endmodule
